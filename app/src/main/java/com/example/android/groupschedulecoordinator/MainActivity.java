3ff7a8d190dddc63b76b9a560c53699ea50589c8
ThisChessPlayer <jasonma5501@gmail.com>
2016-10-05 17:40:31 -0700

    fixed mainActivity code, compiles and displays properly now

--- app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
@@ -6,18 +6,33 @@ import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
   WeekView mWeekView;
   WeekView.EventClickListener mEventClickListener;
   WeekView.EventLongPressListener mEventLongPressListener;
 
-    MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+    WeekView.MonthChangeListener mMonthChangeListener = new WeekView.MonthChangeListener() {
         @Override
         public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
             // Populate the week view with some events.
-            List<WeekViewEvent> events = getEvents(newYear, newMonth);
-            return events;
+          List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+          Calendar startTime = Calendar.getInstance();
+          startTime.set(Calendar.HOUR_OF_DAY, 3);
+          startTime.set(Calendar.MINUTE, 0);
+          startTime.set(Calendar.MONTH, newMonth-1);
+          startTime.set(Calendar.YEAR, newYear);
+          Calendar endTime = (Calendar) startTime.clone();
+          endTime.add(Calendar.HOUR, 1);
+          endTime.set(Calendar.MONTH, newMonth-1);
+          WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+          //event.setColor(getResources().getColor(R.color.event_color_01));
+          events.add(event);
+
+          return events;
         }
     };
 
@@ -37,5 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Set long press listener for events.
     mWeekView.setEventLongPressListener(mEventLongPressListener);
+  }
+
+  protected String getEventTitle(Calendar time) {
+    return String.format("Event of %02d:%02d %s/%d", time.get(Calendar.HOUR_OF_DAY), time.get(Calendar.MINUTE), time.get(Calendar.MONTH)+1, time.get(Calendar.DAY_OF_MONTH));
   }
 }
