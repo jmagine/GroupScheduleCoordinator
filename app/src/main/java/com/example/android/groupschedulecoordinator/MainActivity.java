b1da021dc90a231a01ea41112547bf674dd55e74
Nathaniel Hsu <14hsunathaniel@gmail.com>
2016-10-05 17:26:05 -0700

    UI tests

--- app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.android.groupschedulecoordinator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+  WeekView mWeekView;
+  WeekView.EventClickListener mEventClickListener;
+  WeekView.EventLongPressListener mEventLongPressListener;
+
+    MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+        @Override
+        public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+            // Populate the week view with some events.
+            List<WeekViewEvent> events = getEvents(newYear, newMonth);
+            return events;
+        }
+    };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    // Get a reference for the week view in the layout.
+    mWeekView = (WeekView) findViewById(R.id.weekView);
+
+    // Set an action when any event is clicked.
+    mWeekView.setOnEventClickListener(mEventClickListener);
+
+    // The week view has infinite scrolling horizontally. We have to provide the events of a
+    // month every time the month changes on the week view.
+    mWeekView.setMonthChangeListener(mMonthChangeListener);
+
+    // Set long press listener for events.
+    mWeekView.setEventLongPressListener(mEventLongPressListener);
   }
 }
