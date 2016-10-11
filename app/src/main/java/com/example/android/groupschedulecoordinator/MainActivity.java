3a71d9fc4b2aa38ee449b7b76672cf8f12b673af
Nathaniel Hsu <14hsunathaniel@gmail.com>
2016-10-10 18:14:26 -0700

    List and Fab

--- app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
@@ -4,23 +4,57 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
   Button btn;
+  ListView lv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    btn = (Button)findViewById(R.id.button);
+    lv = (ListView) findViewById(R.id.groupList);
 
-    btn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        startActivity(new Intent(MainActivity.this, Main0Activity.class));
-      }
-    });
+    List<String> group_list = new ArrayList<String>();
+    group_list.add("foo");
+    group_list.add("bar");
+
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+          this,
+          android.R.layout.simple_list_item_1,
+          group_list );
+
+    lv.setAdapter(arrayAdapter);
+
+      lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view, int position,
+                                  long id) {
+
+              String item = ((TextView)view).getText().toString();
+
+              Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+              startActivity(new Intent(MainActivity.this, Main0Activity.class));
+          }
+      });
+
+//    btn = (Button)findViewById(R.id.button);
+//
+//    btn.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        startActivity(new Intent(MainActivity.this, Main0Activity.class));
+//      }
+//    });
   }
 }
