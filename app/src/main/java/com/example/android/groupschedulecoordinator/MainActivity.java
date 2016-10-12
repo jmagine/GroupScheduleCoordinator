06d7ae34f075e44a8a3f05cda2b07171b3beca82
Jeremy <jeremyfolk@gmail.com>
2016-10-12 01:43:49 -0700

    Fabs

--- app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.groupschedulecoordinator;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,38 +16,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-  Button btn;
-  ListView lv;
+    Button btn;
+    ListView lv;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    lv = (ListView) findViewById(R.id.groupList);
+        lv = (ListView) findViewById(R.id.groupList);
 
-    List<String> group_list = new ArrayList<String>();
-    group_list.add("foo");
-    group_list.add("bar");
+        List<String> group_list = new ArrayList<String>();
+        group_list.add("foo");
+        group_list.add("bar");
 
-    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-          this,
-          android.R.layout.simple_list_item_1,
-          group_list );
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                group_list);
 
-    lv.setAdapter(arrayAdapter);
+        lv.setAdapter(arrayAdapter);
 
-      lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-          @Override
-          public void onItemClick(AdapterView<?> parent, View view, int position,
-                                  long id) {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
 
-              String item = ((TextView)view).getText().toString();
+                String item = ((TextView) view).getText().toString();
 
-              Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
-              startActivity(new Intent(MainActivity.this, Main0Activity.class));
-          }
-      });
+                Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this, Main0Activity.class));
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.myFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                android.content.Context context = getApplicationContext();
+                CharSequence text = "Text!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+
+        });
+
 
 //    btn = (Button)findViewById(R.id.button);
 //
@@ -56,5 +71,5 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(new Intent(MainActivity.this, Main0Activity.class));
 //      }
 //    });
-  }
+    }
 }
