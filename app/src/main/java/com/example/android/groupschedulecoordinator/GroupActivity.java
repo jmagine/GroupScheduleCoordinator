0ba0d83c422aa7423328825eed07eb6e34683cd7
Jeremy <jeremyfolk@gmail.com>
2016-10-22 10:07:53 -0700

    Add members in group detail with bug

--- app/src/main/java/com/example/android/groupschedulecoordinator/GroupActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/GroupActivity.java
@@ -1,20 +1,49 @@
 package com.example.android.groupschedulecoordinator;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TabHost;
+
+import java.util.ArrayList;
 
 public class GroupActivity extends AppCompatActivity {
 
+    ListView lv;
+    ArrayList<String> group_list;
+    final Context c = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+
+        lv = (ListView) findViewById(R.id.groupList);
+        group_list = new ArrayList<String>();
+
+        if(group_list != null) {
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    group_list);
+
+            lv.setAdapter(arrayAdapter);
+        }
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -42,6 +71,52 @@ public class GroupActivity extends AppCompatActivity {
         spec.setContent(R.id.calendar);
         spec.setIndicator("Calendar");
         host.addTab(spec);
+
+
+
+
+        Button fab = (Button) findViewById(R.id.addmembers);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.dialog_popup, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.enterEmail);
+
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                group_list.add(userInputDialogEditText.getText().toString());
+                                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                                        c,
+                                        android.R.layout.simple_list_item_1,
+                                        group_list);
+
+                                lv.setAdapter(arrayAdapter);
+                            }
+                        })
+
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        //dialogBox.cancel();
+                                    }
+                                });
+
+
+
+//                Intent intent = new Intent(MainActivity.this, ActivityCreateGroup.class);
+//                intent.putStringArrayListExtra("groupList", group_list);
+//
+//                startActivity(intent);
+                //startActivity(new Intent(MainActivity.this, ActivityCreateGroup.class));
+            }
+
+        });
         
     }
 }
