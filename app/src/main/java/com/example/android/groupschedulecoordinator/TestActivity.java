56ea4c0ecf86a71f3fe143a3021fd4c478cfef7c
Alex Pu <alexander.f.pu@gmail.com>
2016-10-27 10:24:37 -0700

    From a date, find all busy times updated

--- app/src/main/java/com/example/android/groupschedulecoordinator/TestActivity.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/TestActivity.java
@@ -428,7 +428,7 @@ public class TestActivity extends Activity
 
         @Override
         protected void onPostExecute(List<String> output) {
-            mProgress.hide();
+            mProgress.dismiss();
             if (output == null || output.size() == 0) {
                 mOutputText.setText("No results returned.");
             } else {
@@ -439,7 +439,7 @@ public class TestActivity extends Activity
 
         @Override
         protected void onCancelled() {
-            mProgress.hide();
+            mProgress.dismiss();
             if (mLastError != null) {
                 if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
                     showGooglePlayServicesAvailabilityErrorDialog(
