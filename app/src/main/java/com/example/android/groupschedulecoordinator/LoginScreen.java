1caadca196e9c87ed6a41c5c448173fccda029d3
Tommy <tommyt1109@gmail.com>
2016-11-02 23:40:14 -0700

    changed back to R.string.default_web_client_id

--- app/src/main/java/com/example/android/groupschedulecoordinator/LoginScreen.java
+++ app/src/main/java/com/example/android/groupschedulecoordinator/LoginScreen.java
@@ -77,7 +77,7 @@ public class LoginScreen extends AppCompatActivity{
         };
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
