0d8261f0a0ba406acc993ec3d40827abe8515af1
Patrick <patricklt96@gmail.com>
2016-11-27 19:52:00 -0800

    Created unit tests for User (minus Parcelable methods.) Minor change to acceptGroup()

--- app/src/test/java/TestSuite/suite.java
+++ app/src/test/java/TestSuite/suite.java
@@ -7,12 +7,13 @@ package TestSuite;
 import com.example.android.groupschedulecoordinator.EventTests;
 import com.example.android.groupschedulecoordinator.FreeTimeCalculatorTests;
 import com.example.android.groupschedulecoordinator.GroupTests;
+import com.example.android.groupschedulecoordinator.UserTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 // Runs all unit tests
 @RunWith(Suite.class)
-@Suite.SuiteClasses({EventTests.class, FreeTimeCalculatorTests.class, GroupTests.class})
+@Suite.SuiteClasses({EventTests.class, FreeTimeCalculatorTests.class, GroupTests.class, UserTests.class})
 public class suite {
 
 }
